eb706256f02eeeb1f0f3c9c42135f1bd11db3649
yachuan.chen <chenyachuan@hotmail.com>
2013-02-22 18:07:32 +0800

    初始化项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miamir::Application.config.secret_token = '7be84cb9c9c45cef2915f0c4a2d4fcc020c44ef278a0780c3872f253b15beb594b846fca4f17a05bf1d05f7fbef76096c2d7022632cbf4b70e56c6b17cf7abc8'
