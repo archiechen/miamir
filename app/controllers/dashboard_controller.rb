746a4fff87ae1ec135744b9938b2f67492ec2f3f
yachuan.chen <chenyachuan@hotmail.com>
2013-03-15 11:09:03 +0800

    处理current team为空的情况

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,18 @@
 class DashboardController < ApplicationController
 
   def index
-    @ready_tasks = @current_team.tasks.where(:status=>'Ready').all
-    @progress_tasks = @current_team.tasks.where(:status=>'Progress').all
-    @done_tasks = @current_team.tasks.where(:status=>'Done').all
+    if @current_team
+      @ready_tasks = @current_team.tasks.where(:status=>'Ready').all
+      @progress_tasks = @current_team.tasks.where(:status=>'Progress').all
+      @done_tasks = @current_team.tasks.where(:status=>'Done').all
+    end
   end
 
   def planning
-    @ready_tasks = @current_team.tasks.where(:status=>'Ready').all
-    @backlog_tasks = @current_team.tasks.where(:status=>'New').all
+    if @current_team
+      @ready_tasks = @current_team.tasks.where(:status=>'Ready').all
+      @backlog_tasks = @current_team.tasks.where(:status=>'New').all
+    end
   end
 
 
