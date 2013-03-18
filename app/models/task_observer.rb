b91df3face74b87cd5b02dbf036cd1b449531923
yachuan.chen <chenyachuan@hotmail.com>
2013-03-18 19:16:48 +0800

    更新redmine地址

--- app/models/task_observer.rb
+++ app/models/task_observer.rb
@@ -4,7 +4,7 @@ class TaskObserver < ActiveRecord::Observer
   def before_create(task)
     if !User.current.redmine_key.nil?
       puts Redmine::Issue.new(task).to_json
-      uri = URI.parse("http://localhost:3000/issues.json")
+      uri = URI.parse("http://192.168.6.180/redmine/issues.json")
       http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Post.new(uri.request_uri)
       request.body = Redmine::Issue.new(task).to_json
@@ -24,7 +24,7 @@ class TaskObserver < ActiveRecord::Observer
 
   def before_destroy(task)
     if !task.redmine_issue_id.nil? and !User.current.redmine_key.nil?
-      uri = URI.parse("http://localhost:3000/issues/"+task.redmine_issue_id.to_s+".json")
+      uri = URI.parse("http://192.168.6.180/redmine/issues/"+task.redmine_issue_id.to_s+".json")
       http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Delete.new(uri.request_uri)
       request["content-type"] = "application/json"
