9d075716f8b8f3f9684de31f14860f202988cde8
yachuan.chen <chenyachuan@hotmail.com>
2013-03-26 11:25:28 +0800

    ready状态的任务卡也可以显示头像

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -2,7 +2,9 @@ require 'rubygems'
 require 'rufus/scheduler'  
 scheduler = Rufus::Scheduler.start_new
 scheduler.cron '10 23 * * *' do
+  puts "job starting ..."
   # every day of the week at 22:00 (10pm)
   Task.emptying()
   Team.burning()
+  puts "job ending ..."
 end
