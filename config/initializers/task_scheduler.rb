cf0550cf340fc960456e4860f4dc3ba6a579951c
yachuan.chen <chenyachuan@hotmail.com>
2013-03-28 14:04:26 +0800

    使用Rails.logger输出日志

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,10 +1,10 @@
 require 'rubygems'
 require 'rufus/scheduler'  
 scheduler = Rufus::Scheduler.start_new
-scheduler.cron '10 23 * * *' do
-  puts "job starting ..."
+scheduler.cron '01 14 * * 1-5' do
+  Rails.logger.info "job starting ..."
   # every day of the week at 22:00 (10pm)
   Task.emptying()
   Team.burning()
-  puts "job ending ..."
+  Rails.logger.info "job ending ..."
 end
