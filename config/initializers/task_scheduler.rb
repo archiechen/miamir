b7e95279ea3167fbe74bea9372a1f378dccec189
yachuan.chen <chenyachuan@hotmail.com>
2013-03-28 13:31:50 +0800

    测试rufus

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'rufus/scheduler'  
 scheduler = Rufus::Scheduler.start_new
-scheduler.cron '20 11 * * 1-5' do
+scheduler.cron '50 * * * 1-5' do
   Rails.logger.info "job starting ..."
   # every day of the week at 22:00 (10pm)
   #Task.emptying()
