23c6e19cb6033a77a07fdc2fa12b25478e0170a4
yachuan.chen <chenyachuan@hotmail.com>
2013-03-29 22:13:16 +0800

    登录页面不显示人和功能菜单，只有登录相关的表单和链接

--- config/application.rb
+++ config/application.rb
@@ -71,5 +71,13 @@ module Miamir
     
     ENV["GMAIL_USER"] = MAIL_CONFIG['username']
     ENV["GMAIL_PASSWORD"] = MAIL_CONFIG['password']
+
+    config.to_prepare do
+      Devise::SessionsController.layout "devise"
+      Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "devise" }
+      Devise::ConfirmationsController.layout "devise"
+      Devise::UnlocksController.layout "devise"            
+      Devise::PasswordsController.layout "devise"        
+    end
   end
 end
