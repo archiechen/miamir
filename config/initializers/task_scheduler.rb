35283fbd1ea65d2b35fdcbf3e41d90693e6d3575
yachuan.chen <chenyachuan@hotmail.com>
2013-04-07 13:49:57 +0800

    周末也创建燃尽数据

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'rufus/scheduler'  
 scheduler = Rufus::Scheduler.start_new
-scheduler.cron '10 23 * * 1-5' do
+scheduler.cron '10 23 * * 1-7' do
   Rails.logger.info "job starting ..."
   # every day of the week at 22:00 (10pm)
   Task.emptying()
