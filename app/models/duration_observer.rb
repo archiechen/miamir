57aef5e0c06e96deabfe3bacbee1fb6949a87297
yachuan.chen <chenyachuan@hotmail.com>
2013-04-23 10:28:42 +0800

    duration observer增加异常处理

--- app/models/duration_observer.rb
+++ app/models/duration_observer.rb
@@ -5,11 +5,16 @@ class DurationObserver < ActiveRecord::Observer
     time_entry = Redmine::TimeEntry.new(duration)
     if time_entry.hours>0 and !duration.owner.redmine_key.nil?
       response_body = Redmine::Helper.create_time_entry(time_entry,duration.owner.redmine_key)
-      duration.logger.info(JSON.parse(response_body))
+      begin
+        Rails.logger.info(JSON.parse(response_body))
 
-      if !duration.partner.nil? and !duration.partner.redmine_key.nil?
-        response_body = Redmine::Helper.create_time_entry(time_entry,duration.partner.redmine_key)
-        duration.logger.info(JSON.parse(response_body))
+        if !duration.partner.nil? and !duration.partner.redmine_key.nil?
+          response_body = Redmine::Helper.create_time_entry(time_entry,duration.partner.redmine_key)
+          Rails.logger.info(JSON.parse(response_body))
+        end
+      rescue => e
+        Rails.logger.warn(e.class)
+        Rails.logger.warn(response_body)
       end
     end
   end
